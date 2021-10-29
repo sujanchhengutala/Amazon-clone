125e1e75d033b023432c56f8d9eff8c47dcbf157
sujanchhengutala <47767420+sujanchhengutala@users.noreply.github.com>
2021-10-29 13:53:48 +0545

    first day install react and install material ui and import

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDJtnVJUr8_O6za1fDcdZFDlRgkYD2WHSM",
+    authDomain: "practice-c26ec.firebaseapp.com",
+    projectId: "practice-c26ec",
+    storageBucket: "practice-c26ec.appspot.com",
+    messagingSenderId: "968700463603",
+    appId: "1:968700463603:web:2b0ebbcf30231d95b9ea86",
+    measurementId: "G-JP6FRD8M96"
+  };
